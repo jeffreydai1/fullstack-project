87aefa9a0df4b69dfcd32dc3665faaf1e000ddc5
Jeffrey Dai <jeffrey_dai@brown.edu>
2021-03-16 04:07:25 -0400

    super basic login, no database yet

--- puzzles/sitescript.js
+++ puzzles/sitescript.js
@@ -2,35 +2,47 @@
 const express = require('express')
 const app = express();
 const port = 3000;
+var cookieParser = require('cookie-parser');
+app.use(cookieParser());
 app.use(express.static(__dirname + '/statics'));
+app.use(express.urlencoded({ extended: true }));
+app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/puzzles.html');
+    const user = req.cookies.username;
+    res.render(__dirname + '/puzzles.ejs', { user: user });
 })
 
 app.get('/puzzles', (req, res) => {
-    res.sendFile(__dirname + '/puzzles.html');
+    const user = req.cookies.username;
+    res.render(__dirname + '/puzzles.ejs', { user: user });
 })
 
 app.get('/ranking', (req, res) => {
-    res.sendFile(__dirname + '/ranking.html');
+    res.render(__dirname + '/ranking.ejs');
 })
 
 app.get('/your%20score', (req, res) => {
-    res.sendFile(__dirname + '/score.html');
+    res.render(__dirname + '/score.ejs');
 })
 
+app.post('/login', (req, res) => {
+    res.clearCookie('username');
+    res.clearCookie('name');
+    const username = req.body.username;
+    const password = req.body.password;
+    if (username == 'bruh' && password == 'pass') {
+        res.cookie('username', 'bruh')
+        const user = req.cookies.username;
+        res.render(__dirname + '/puzzles.ejs', { user: 'bruh' });
+    }
+    else {
+        res.clearCookie('username');
+        res.clearCookie('name');
+        res.render(__dirname + '/puzzles.ejs', { user: null });
+    }
+});
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
 })
-
-/*
-app.post('/login',
-    passport.authenticate('local', {
-        successRedirect: '/',
-        failureRedirect: '/login',
-        failureFlash: true
-    })
-);
-*/
