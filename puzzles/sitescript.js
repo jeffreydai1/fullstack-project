85ab34b593396aa1f6a03439549e2604b6eedf12
Your Name <kevin_lu2@brown.edu>
2021-03-16 22:43:19 -0400

    connected to mongodb database, added ranking table

--- puzzles/sitescript.js
+++ puzzles/sitescript.js
@@ -2,15 +2,32 @@
 const express = require('express')
 const app = express();
 const port = 3000;
+const MongoClient = require('mongodb').MongoClient;
+const ObjectID = require('mongodb').ObjectID;
 var cookieParser = require('cookie-parser');
 app.use(cookieParser());
 app.use(express.static(__dirname + '/statics'));
 app.use(express.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
 
+//mongodb vars
+const url = 'mongodb+srv://ourgroup:fullstackatbrown@cluster0.urser.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+const dbName = 'puzzlesitedata'
+
 app.get('/', (req, res) => {
     const user = req.cookies.username;
-    res.render(__dirname + '/puzzles.ejs', { user: user });
+    MongoClient.connect(url, function (err, client) {
+      if (err) throw err
+      var db = client.db(dbName);
+
+      db.collection('psd').find().toArray(function (err, result) {
+        if (err) throw err
+
+        //ISSUE: ONLY WORKS IF YOU START WITH SITESCRIPT.JS, GOING TO PUZZLES.EJS WITHOUT GOING THROUGH
+        //SITESCRIPT.JS WILL NOT LOAD THE PAGE. 
+        res.render(__dirname + '/puzzles.ejs', { user: user, records: result });
+      })
+    })
 })
 
 app.get('/puzzles', (req, res) => {
@@ -19,7 +36,7 @@ app.get('/puzzles', (req, res) => {
 })
 
 app.get('/ranking', (req, res) => {
-    res.render(__dirname + '/ranking.ejs');
+    res.render(__dirname + '/ranking.ejs', );
 })
 
 app.get('/your%20score', (req, res) => {
